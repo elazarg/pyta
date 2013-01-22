753997c5478c2241006910ba11bba91cb00ba3dd
elazarg <elazarg@localhost>
2013-01-22 18:25:42 +0200

    types module split

---
+++ typeset.py
@@ -0,0 +1,59 @@
+
+def st(t):
+    return TypeSet({t})
+
+class AnyClass:
+    def __init__(self):
+        super(type)
+        
+    def __repr__(self):
+        return "Any"
+    
+    def can_split_to(self, n):
+        return True
+        
+    def split_to(self, n):
+        return [{Any}] * n
+
+Any = AnyClass()
+
+class TypeSet:
+    
+    def __init__(self, iterable):
+        assert not isinstance(iterable, TypeSet)
+        self.types = set(iterable)
+        self.to_invariant()
+        
+    def update(self, iterable):
+        self.types.update(iterable)
+        self.to_invariant()
+        
+    def add(self, obj):
+        assert not isinstance(obj, TypeSet)
+        self.types.add(obj)
+        self.to_invariant()
+
+    def union(self, other):
+        assert isinstance(other, TypeSet)
+        return TypeSet(set.union(self.types, other.types)) 
+    
+    def to_invariant(self):
+        if Any in self.types:
+            self.types = {Any}
+    
+    def __iter__(self):
+        return self.types.__iter__()
+    
+    @staticmethod
+    def union_all(iterable):
+        if len(iterable)==0:
+            return TypeSet({})
+        return TypeSet(set.union(*[i.types for i in iterable]))
+    
+    def __repr__(self):
+        return 'T{' +', '.join([str(i) for i in self.types]) +'}'
+
+    def __len__(self):
+        return len(self.types)
+ 
+Empty = TypeSet({})
